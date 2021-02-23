674a2ecad424dd13895bf9f06e24ff5b97503a16
Juan Manuel De Senzi <juanmadesenzi@gmail.com>
2021-02-23 16:26:46 -0300

    2,3,4 finalizados

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* $('#index__main__div1 a').on('mouseover', function(){
+    $(this).animate({
+        fontSize: "26pt"
+    }, 200)
+});
+
+$('#index__main__div1 a').on('mouseout', function(){
+    $(this).animate({
+        fontSize: "24pt"
+    },200)
+}) */
