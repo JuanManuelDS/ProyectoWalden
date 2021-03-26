d7dee0642aa46d974c6c2a6d305e89e46f8aab7f
Juan Manuel De Senzi <juanmadesenzi@gmail.com>
2021-03-26 12:40:00 -0300

    before merging

--- js/responsive.js
+++ js/responsive.js
@@ -1,5 +1,10 @@
 $(()=>{
     if($(document).width()<=850){
-        
+        let carLi = $(".car-li"),
+        carLiClon = carLi.clone(),
+        carUl = $('<ul class="car-ul"></ul>');
+      carLiClon.appendTo(carUl);
+      carUl.appendTo("nav div");
+      carLi.remove();
     }
 });
